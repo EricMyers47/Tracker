1d53842347f06825b124ee052cc04f0588468121
Eric Myers <myers@EricMyers.com>
2017-03-26 09:19:24 -0400

    Initial Commit

---
+++ Tracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tracker
+//
+//  Created by Eric Myers on 3/26/17.
+//  Copyright © 2017 EricMyers.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
